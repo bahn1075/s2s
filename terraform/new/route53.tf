716db0649135dd7cf3326d8b76c1e66b673eb756
bahn1075 <bahn1075@gmail.com>
2025-02-18 12:46:14 +0900

    ㄱㄱㄱ

--- terraform/new/route53.tf
+++ terraform/new/route53.tf
@@ -3,6 +3,15 @@ resource "aws_route53_zone" "cozy_zone" {
   name = "cozy.tf-dunn.link"
 }
 
+# 상위 도메인에 NS 레코드 등록
+resource "aws_route53_record" "cozy_ns_record" {
+  zone_id = "상위_도메인_호스팅_영역_ID"  # 상위 도메인(tf-dunn.link)의 호스팅 영역 ID를 입력하세요
+  name    = "cozy.tf-dunn.link"
+  type    = "NS"
+  ttl     = 300
+  records = aws_route53_zone.cozy_zone.name_servers
+}
+
 # Route 53 레코드 생성 (Jenkins)
 resource "aws_route53_record" "jenkins_record" {
   zone_id = aws_route53_zone.cozy_zone.zone_id
