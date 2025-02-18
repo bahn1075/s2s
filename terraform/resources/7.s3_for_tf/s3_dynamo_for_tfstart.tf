48e6a7d07c53256f2f79ba5f59a1b2026c183d5b
bahn1075 <bahn1075@gmail.com>
2025-02-18 17:37:26 +0900

    폴더 재지정정

---
+++ terraform/resources/7.s3_for_tf/s3_dynamo_for_tfstart.tf
@@ -0,0 +1,41 @@
+# S3 버킷 생성 (Terraform 상태 파일 저장용)
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "cozy-terraform-state-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name    = "cozy-terraform-state-bucket"
+    Creator = "cozy"
+  }
+}
+
+# DynamoDB 테이블 생성 (Terraform 상태 잠금용)
+resource "aws_dynamodb_table" "terraform_lock_table" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name    = "terraform-lock-table"
+    Creator = "cozy"
+  }
+}
+
+# Terraform 백엔드 설정
+terraform {
+  backend "s3" {
+    bucket         = "cozy-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
