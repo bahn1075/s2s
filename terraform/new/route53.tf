b2b2959b9fc704f0668cac8e70e41d6471f5c945
bahn1075 <bahn1075@gmail.com>
2025-02-18 10:36:38 +0900

    아 존나 힘들다다

--- terraform/new/route53.tf
+++ terraform/new/route53.tf
@@ -29,58 +29,44 @@ resource "aws_route53_record" "gitlab_record" {
   }
 }
 
+# ACM 퍼블릭 인증서 요청
+resource "aws_acm_certificate" "cozy_cert" {
+  domain_name       = "cozy.tf-dunn.link"
+  validation_method = "DNS"
 
-# ec2 대상 그룹 생성
-resource "aws_lb_target_group" "cozy_jenkins_tg" {
-  name     = "cozy-jenkins-tg"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.cozy_vpc.id
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    matcher             = "200"
-  }
+  subject_alternative_names = [
+    "jenkins.cozy.tf-dunn.link",
+    "gitlab.cozy.tf-dunn.link"
+  ]
 
   tags = {
-    Name    = "cozy-jenkins-tg"
+    Name    = "cozy-cert"
     Creator = "cozy"
   }
-}
 
-resource "aws_lb_target_group_attachment" "cozy_jenkins_tg_attachment" {
-  target_group_arn = aws_lb_target_group.cozy_jenkins_tg.arn
-  target_id        = aws_instance.cozy-jenkins.id
-  port             = 8080
-}
-
-resource "aws_lb_target_group" "cozy_gitlab_tg" {
-  name     = "cozy-gitlab-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.cozy_vpc.id
-
-  health_check {
-    path                = "/login"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    matcher             = "200"
-  }
-
-  tags = {
-    Name    = "cozy-gitlab-tg"
-    Creator = "cozy"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_lb_target_group_attachment" "cozy_gitlab_tg_attachment" {
-  target_group_arn = aws_lb_target_group.cozy_gitlab_tg.arn
-  target_id        = aws_instance.cozy-gitlab.id
-  port             = 80
+# Route 53 레코드 생성 (ACM 인증서 검증)
+resource "aws_route53_record" "cozy_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cozy_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.cozy_zone.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cozy_cert_validation" {
+  certificate_arn         = aws_acm_certificate.cozy_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cozy_cert_validation : record.fqdn]
 }
