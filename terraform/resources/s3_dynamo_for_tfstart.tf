6412b0aef2610c226dd6a1cfa0224544bd944ab8
bahn1075 <bahn1075@gmail.com>
2025-02-19 11:16:51 +0900

    다시 한곳으로 다 모음음

---
+++ terraform/resources/s3_dynamo_for_tfstart.tf
@@ -0,0 +1,34 @@
+# S3 버킷 생성 (Terraform 상태 파일 저장용)
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "cozy-terraform-state-bucket"
+
+  tags = {
+    Name    = "cozy-terraform-state-bucket"
+    Creator = "cozy"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# DynamoDB 테이블 생성 (Terraform 상태 잠금용)
+resource "aws_dynamodb_table" "terraform_lock_table" {
+  name         = "cozy-terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name    = "cozy-terraform-lock-table"
+    Creator = "cozy"
+  }
+}
